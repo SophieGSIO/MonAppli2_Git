7640086174f04769186a549c3fa050ddf88d7c74
SG <sophie.girard@ac-grenoble.fr>
2017-11-26 19:09:31 +0100

    Modif de SG

--- MonAppli2/Program.cs
+++ MonAppli2/Program.cs
@@ -13,6 +13,7 @@ namespace MonAppli2
             Console.WriteLine("Hello world");
             Class1 objet = new Class1(1, "Dupont");
             // ajout commentaire par Autre util
+            // SG modif
             Console.ReadLine();
         }
     }
